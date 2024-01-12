731f5676eee84c1eb2a4a6da10746510731465fc
Kat <kathy.uc.17@gmail.com>
2024-01-12 00:53:56 -0500

    Adding restaurantsRouter to index.ts and errorHandler

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,30 @@ import { authRouter } from "./users/routers/auth.router";
 import { userRouter } from "./users/routers/user.router";
 import { ZodError } from "zod";
 import ExpressReviewsError from "./utils/error/ExpressReviewsError";
+import restaurantsRouter from "./restaurants/routers/restaurant.router";
+import { formatErrors } from "./utils/formatErrors.utils";
 
 const app = express();
 const port = 5500;
 app.use(express.json());
+
 app.use(authRouter);
 app.use("/users", userRouter);
+app.use("/restaurants", restaurantsRouter);
+
 app.use(
   (
     error: Error | ZodError | ExpressReviewsError,
-    _req: Request,
+    req: Request,
     res: Response,
-    _next: NextFunction
+    next: NextFunction
   ) => {
     if (error instanceof ZodError) {
       res.status(400).json({
         ok: false,
-        message: "wrong type of fields",
-        details: error.formErrors.fieldErrors,
+        message: "Error en validación",
+        details: formatErrors(error.formErrors.fieldErrors),
       });
-      return;
     } else if (error instanceof ExpressReviewsError) {
       res.status(error.statusCode).json({
         ok: false,
@@ -35,11 +39,14 @@ app.use(
         },
       });
     } else {
-      res.status(500).send({
+      res.status(500).json({
         ok: false,
-        message: error.message,
+        error: {
+          message: "Error interno del servidor",
+        },
       });
     }
   }
 );
+
 app.listen(port, () => console.log(`Escuchando al puerto ${port}`));
