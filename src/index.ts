972cabbfa767b917f5759cb42a75ec1740e5bfb3
Kat <kathy.uc.17@gmail.com>
2024-01-12 00:53:56 -0500

    Adding restaurantsRouter to index.ts and errorHandler

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,40 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { authRouter } from "./users/routers/auth.router";
 import { userRouter } from "./users/routers/user.router";
+import { ZodError } from "zod";
+import ExpressReviewsError from "./utils/error/ExpressReviewsError";
+import restaurantsRouter from "./restaurants/routers/restaurant.router";
+import { formatErrors } from "./utils/formatErrors.utils";
 
 const app = express();
 const port = 5500;
 app.use(express.json());
-app.use(authRouter)
-app.use("/users", userRouter)
+
+app.use(authRouter);
+app.use("/users", userRouter);
+app.use("/restaurants", restaurantsRouter);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof ZodError) {
+    res.status(400).json({
+      ok: false,
+      message: "Error en validación",
+      details: formatErrors(error.formErrors.fieldErrors),
+    });
+  } else if (error instanceof ExpressReviewsError) {
+    res.status(error.statusCode).json({
+      ok: false,
+      message: error.message,
+      details: error.details,
+    });
+  } else {
+    res.status(500).json({
+      ok: false,
+      error: {
+        message: "Error interno del servidor",
+      },
+    });
+  }
+});
+
 app.listen(port, () => console.log(`Escuchando al puerto ${port}`));
