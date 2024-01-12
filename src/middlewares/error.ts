b4523c1ee894afeb865e54c3e9a98059c8c31e90
Kat <kathy.uc.17@gmail.com>
2024-01-12 10:35:11 -0500

    Adding errorHandler middleware and modifying service layer

---
+++ src/middlewares/error.ts
@@ -0,0 +1,32 @@
+import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+import { formatErrors } from "../utils/formatErrors.utils";
+import ExpressReviewsError from "../utils/error/ExpressReviewsError";
+
+export default function errorHandler(
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  if (error instanceof ZodError) {
+    res.status(400).json({
+      ok: false,
+      message: "Error en validación",
+      details: formatErrors(error.formErrors.fieldErrors),
+    });
+  } else if (error instanceof ExpressReviewsError) {
+    res.status(error.statusCode).json({
+      ok: false,
+      message: error.message,
+      details: error.details,
+    });
+  } else {
+    res.status(500).json({
+      ok: false,
+      error: {
+        message: "Error interno del servidor",
+      },
+    });
+  }
+}
