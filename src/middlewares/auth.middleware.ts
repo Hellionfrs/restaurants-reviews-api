c1dc80129b8a7408de17923290bd73ea6308f8b2
fred <fredy.rodriguez@pucp.pe>
2024-01-11 23:36:49 -0500

    implemented autheticateHandler as a middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,26 +2,41 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import ExpressReviewsError from "../utils/error/ExpressReviewsError";
 
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: number;
+      userRole?: string;
+    }
+  }
+}
 const jwtSecret = "ultra-secret";
 
 export const authenticateHandler = async (
   req: Request,
-  res: Response,
+  _res: Response,
   next: NextFunction
 ) => {
   const token = req.headers.authorization?.split(" ")[1] || "";
-  let userId;
-
+  if (!token) {
+    return next(
+      new ExpressReviewsError(
+        "No autorizado",
+        401,
+        "error on authenticateHandler middleware"
+      )
+    );
+  }
   try {
     const payload = jwt.verify(token, jwtSecret) as {
       userId: number;
+      userRole: string;
       iat: number;
       exp: number;
     };
-    userId = payload.userId;
-    if (!userId) {
-      next("error");
-    }
+    req.userId = payload.userId;
+    req.userRole = payload.userRole;
+    next();
   } catch (error) {
     new ExpressReviewsError("no autorizado", 401, "controller error");
   }
